3d41352a5edc15b9f0e2a7c5e389256f30bf9fe5
EsmaNErdem <esmaerdem94@gmail.com>
2024-03-29 19:48:19 -0400

    migrating db and creating task db model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+PRIORITY_CHOICES = [
+    (1, 'Low'),
+    (2, 'Medium'),
+    (3, 'High'),
+]
+
+class Task(models.Model):
+    # taskOwner = models.ForeignKey(User, on_delete=models.CASCADE)    taskName = models.CharField(max_length=100)
+    dueDate = models.DateField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+
