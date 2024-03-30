e5d1251a03adfb3f69b43e4b1580b6a8188074b5
EsmaNErdem <esmaerdem94@gmail.com>
2024-03-29 20:20:46 -0400

    migrating db and creating task db model

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tasks.apps.TasksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
