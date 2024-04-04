94e9d6c3946c2fbef4bf93f168e3ca8d3b1233d7
EsmaNErdem <esmaerdem94@gmail.com>
2024-04-04 00:58:04 -0400

    creating super user, need to fix db model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 PRIORITY_CHOICES = [
-    (1, 'Low'),
-    (2, 'Medium'),
-    (3, 'High'),
+    (1, 'Can wait'),
+    (2, 'Important'),
+    (3, 'Get to work'),
 ]
 
 class Task(models.Model):
-    # taskOwner = models.ForeignKey(User, on_delete=models.CASCADE)    taskName = models.CharField(max_length=100)
+    # taskOwner = models.ForeignKey(User, on_delete=models.CASCADE)
+    taskName = models.CharField(max_length=100, default='task')
     dueDate = models.DateField()
-    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    priority = models.CharField(choices=PRIORITY_CHOICES, default=1)
 
+    def __str__(self):
+        """String representation of table column"""
+
+        return f"{self.taskName}, due by {self.dueDate} with {self.priority} priority"
